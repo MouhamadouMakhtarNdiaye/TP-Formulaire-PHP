ca77f854aa5850eed121aa435a432cef2631ad7c
Makhtar <m0uhamadoundiay3@gmail.com>
2026-04-12 02:12:36 +0100

    first commit

---
+++ Tp_Candidature.php
@@ -0,0 +1,17 @@
+<?php
+$prenom    = '';
+$nom       = '';
+$email     = '';
+$age       = '';
+$filiere   = '';
+$motivation = '';
+$erreurs   = [];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Formulaire de Candidature  </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
